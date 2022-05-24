cb90f306bf82fcdf9e3408ed6b88ed03912a6f71
Gustavo Reis <gustavohdosreis@gmail.com>
2022-05-24 11:26:13 -0300

    initial config

---
+++ src/main/java/br/com/luminaapps/manutencaobike/ManutencaoBikeApplication.java
@@ -0,0 +1,13 @@
+package br.com.luminaapps.manutencaobike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoBikeApplication.class, args);
+	}
+
+}
